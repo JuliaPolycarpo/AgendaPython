9c0cf2fc7a4dd0b2e7a115ebdc02637bf993f823
JuliaPolycarpo <polycarpo9@gamil.com>
2026-02-02 16:46:53 -0300

    Inserindo Banco de Dados

--- teste.py
+++ teste.py
@@ -1,1 +1,9 @@
-print("Hello, Word!")
+from models.database import Database
+
+#Teste Gerenciador de Contexto 
+with Database('./data/tarefas.sqlite3') as db:
+    db.executar('INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);', 
+                    ('Usar o gerenciador de contexto', '2026-02-03')
+                )
+    
+
